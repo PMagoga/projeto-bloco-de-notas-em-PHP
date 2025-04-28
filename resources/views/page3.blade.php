4eae04bf0dc70b99b008dda1775465aa5be5d281
PMagoga <pmagoga@gmail.com>
2025-04-28 11:37:35 -0300

    adicionando views

--- resources/views/page3.blade.php
+++ resources/views/page3.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts/main_layout')
 @section('content')
-    <h1>Welcome View and Blade</h1>
+    <h1>Page 3</h1>
     <hr>
     <h3>O valor é <?php echo $value ?></h3>    
 @endsection
-
