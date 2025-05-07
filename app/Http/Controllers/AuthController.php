b63ba2a54a8758b0fc32b9b2cc3284eeea58a46f
PMagoga <pmagoga@gmail.com>
2025-05-07 20:00:40 -0300

    atualizando

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,7 +8,24 @@ class AuthController extends Controller
 {
     public function login()
     {
-        echo "login";
+        return view('login');
+    }
+
+    public function loginSubmit(Request $request)
+    {
+        //form validation
+        $request->validate(
+            [
+                'text_username' =>'required',
+                'text_password'=> 'required',
+            ]
+        );
+
+        // get user input
+        $username = $request->input('text_username');
+        $password = $request->input('text_password');
+
+        echo 'OK';
     }
 
     public function logout()
